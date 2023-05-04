4c8faac4b816d1f387352a9fa1285b5cfdb9466d
grnguidi <grnguidi@gmail.com>
2023-05-04 15:02:29 -0300

    contact-listV1.1

---
+++ Contact-list/Contact-list/Contact.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact_list
+{
+    internal class Contact
+    {
+        //internal var contact class
+        private string firstname;
+        private string lastname;
+        private string phone;
+
+        //properties
+        public string FirstName
+        {
+            get { return FirstName;  }
+            set { FirstName = value; }
+        }
+
+        public string LastName
+        { 
+            get { return LastName; } 
+            set { LastName = value; } 
+        }
+
+        public string Phone
+        { 
+            get { return Phone;  }
+            set { Phone = value; } 
+        }   
+    }
+}
