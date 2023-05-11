028779e6ad51971e44eb3dcb28fc428c79ab794f
grnguidi <grnguidi@gmail.com>
2023-05-11 15:52:17 -0300

    contact-listV1.2

--- Contact-list/Contact-list/Form1.Designer.cs
+++ Contact-list/Contact-list/Form1.Designer.cs
@@ -28,20 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.contactlst = new System.Windows.Forms.ListBox();
+            this.nametxt = new System.Windows.Forms.TextBox();
+            this.lastnametxt = new System.Windows.Forms.TextBox();
+            this.numbertxt = new System.Windows.Forms.TextBox();
+            this.addcontactbtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Last Name";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Number";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // contactlst
+            // 
+            this.contactlst.FormattingEnabled = true;
+            this.contactlst.Location = new System.Drawing.Point(192, 12);
+            this.contactlst.Name = "contactlst";
+            this.contactlst.Size = new System.Drawing.Size(227, 160);
+            this.contactlst.TabIndex = 3;
+            // 
+            // nametxt
+            // 
+            this.nametxt.Location = new System.Drawing.Point(74, 14);
+            this.nametxt.Name = "nametxt";
+            this.nametxt.Size = new System.Drawing.Size(69, 20);
+            this.nametxt.TabIndex = 4;
+            // 
+            // lastnametxt
+            // 
+            this.lastnametxt.Location = new System.Drawing.Point(76, 51);
+            this.lastnametxt.Name = "lastnametxt";
+            this.lastnametxt.Size = new System.Drawing.Size(67, 20);
+            this.lastnametxt.TabIndex = 5;
+            // 
+            // numbertxt
+            // 
+            this.numbertxt.Location = new System.Drawing.Point(77, 91);
+            this.numbertxt.Name = "numbertxt";
+            this.numbertxt.Size = new System.Drawing.Size(66, 20);
+            this.numbertxt.TabIndex = 6;
+            // 
+            // addcontactbtn
+            // 
+            this.addcontactbtn.Location = new System.Drawing.Point(15, 117);
+            this.addcontactbtn.Name = "addcontactbtn";
+            this.addcontactbtn.Size = new System.Drawing.Size(128, 55);
+            this.addcontactbtn.TabIndex = 7;
+            this.addcontactbtn.Text = "Add Contact";
+            this.addcontactbtn.UseVisualStyleBackColor = true;
+            this.addcontactbtn.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(699, 450);
+            this.ClientSize = new System.Drawing.Size(431, 471);
+            this.Controls.Add(this.addcontactbtn);
+            this.Controls.Add(this.numbertxt);
+            this.Controls.Add(this.lastnametxt);
+            this.Controls.Add(this.nametxt);
+            this.Controls.Add(this.contactlst);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox contactlst;
+        private System.Windows.Forms.TextBox nametxt;
+        private System.Windows.Forms.TextBox lastnametxt;
+        private System.Windows.Forms.TextBox numbertxt;
+        private System.Windows.Forms.Button addcontactbtn;
     }
 }
 
--- Contact-list/Contact-list/Form1.cs
+++ Contact-list/Contact-list/Form1.cs
@@ -16,5 +16,25 @@ namespace Contact_list
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
